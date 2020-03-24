1d477a35bbe395f4a70ae2c3a867b2939ea57953
Kymberly Ayodeji <51680946+kayodeji@users.noreply.github.com>
2020-03-24 08:35:17 -0500

    added basics of Amazon.java

---
+++ Amazon.java
@@ -0,0 +1,24 @@
+/**  This program displays a window like the Amazon FireStick/FireTV Home screen **/
+* Programmer Name: <<Include your name>>
+* Date: March 24, 2020
+* For Q4D01 Assignment 
+**/
+
+//Include libraries to package below here
+
+public class Amazon
+{
+
+  public static void main(String[] args)
+  {
+  
+  
+  
+  
+  
+  }
+
+
+
+}
+
